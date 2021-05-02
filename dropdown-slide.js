509182041ab38fed67f6673f5d8f758998f43ac7
Dustin Hofstetter <35706290+HofstetterDustin@users.noreply.github.com>
2021-05-02 13:39:10 -0400

    Add files via upload

---
+++ dropdown-slide.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myDropdown").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myDropdown").style.width = "0%";
+}
---
+++ proIndexFunction.js
@@ -0,0 +1,25 @@
+/* onclick functionality */
+
+function selection() {
+    var sel;
+    if (sel = 0) {
+        football = "true";
+    } else if (sel = 1) {
+        baseball = "true";
+    } else if (sel = 2) {
+        basketball = "true";
+    } else if (sel = 3) {
+        hockey = "true";
+    } else if (sel = 4) {
+        horseRacing = "true";
+    } else if (sel = 5) {
+        tennis = "true";
+    } else if (sel = 6) {
+        soccer = "true";
+    } else if (sel = 7) {
+        rugby = "true";
+    } else {
+        sel = "choose";
+    }
+}
+
